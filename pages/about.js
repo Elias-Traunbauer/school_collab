fc8f07c4de666496c84686f8107d8c9ad5e22149
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2022-11-29 17:01:38 +0100

    import fix

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,5 @@
+import Layout from "../components/Layout"
+
 const About = () => {
     return (
         <>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 const Home = () => {
   return (
