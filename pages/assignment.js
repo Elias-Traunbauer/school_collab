bcc9e78a0a867b5acff90cff24ab76ab178338cc
RauschElias <eliaz.rausch@gmail.com>
2022-12-01 10:30:23 +0100

    added layout

--- pages/assignment.js
+++ pages/assignment.js
@@ -3,17 +3,27 @@ import styles from '../styles/assignment.module.css'
 import { useEffect } from 'react';
 
 export default function Assignment(assignment){
+
+    assignment = {
+        subject:"AM",
+        title: "Hello",
+        time: new Date()
+    }
+    console.log(assignment.time)
     return(
-        <div className={styles.container}>
-                <div class="container">
-                 <div class="description"></div>
-                    <div class="title"></div>
-                    <div class="upload"></div>
-                    <div class="Author"></div>
-                    <div class="button"></div>
-                    <div class="Time"></div>
-                    <div class="Subject"></div>
-                    </div>
+        
+        <div className={styles.assignmentcontainer}>
+            <div className={styles.assignmentcard}>
+                <div className={styles.assignmenthead}>
+                    <p>{assignment.subject}</p>
+                </div>
+                <div className={styles.assignmentbody}>
+                    <h2>{assignment.title}</h2>
+                </div>
+                <div className={styles.assignmentfoot}>
+                    <p>{assignment.time.getHours()+ ":" + assignment.time.getMinutes() +":" + assignment.time.getSeconds()}</p>
+                </div>
+            </div>
         </div>
         
     );
