3559b104ad22d4e4ba3960f27ec22018ad2677fc
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2022-12-02 08:53:28 +0100

    typo

--- api/assignment.php
+++ api/assignment.php
@@ -5,7 +5,7 @@ echo '
     "subject": "DBI",
     "title": "JPA Lab 1: Generieren der IDs",
     "description": "Generieren Sie die IDs für die Klassen \"Kunde\" und \"Bestellung\". Die IDs sollen in der Datenbank automatisch generiert werden. Die IDs sollen vom Typ \"Long\" sein.",
-    "deadline": "2023-01-22T23:59:59.999Z",
+    "deadline": "2023-01-22T23:59:59.999Z"
 }
 ';
 
