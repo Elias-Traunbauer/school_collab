fc6b52b3a894b59a5a1aef1b25d2a7180c18d539
RauschElias <eliaz.rausch@gmail.com>
2022-12-02 07:59:16 +0100

    Added Countdown and inproved the layout

--- pages/assignment.js
+++ pages/assignment.js
@@ -1,15 +1,53 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import styles from '../styles/assignment.module.css'
-import { useEffect } from 'react';
 
 export default function Assignment(assignment){
 
+    //mock
     assignment = {
-        subject:"AM",
-        title: "Hello",
-        time: new Date()
+        subject:"DBI",
+        title: "JPA Lab 1: Generieren der IDs",
+        time: new Date(2020, 5, 19, 25, 65)
     }
-    console.log(assignment.time)
+    
+    const rechnen = new Date().getTime() - new Date(2020, 5, 19, 25, 45).getTime();
+    const [datetime, setDateTime] = useState(rechnen);
+
+    function formatDate(milliseconds) {
+        let seconds = milliseconds/1000;
+        let days = Math.floor(seconds/86400);
+        seconds-= days*86400;
+        let hours = Math.floor(seconds/3600);
+        seconds -= hours*3600;
+        let minutes = Math.floor(seconds/60);
+        seconds -= minutes*60;
+
+        return `${padTo2Digits(days)}d ${padTo2Digits(hours)}h ${padTo2Digits(minutes)}m ${padTo2Digits(
+            seconds,
+          )}s`;
+
+    }
+
+    function padTo2Digits(num) {
+        return num.toString().padStart(2, '0');
+    }
+
+
+
+    useEffect(()=>{
+        //setDatetime(assignment.time)
+        const id = setInterval(() => {
+            if(Math.floor(datetime/1000) != 0)
+            setDateTime(datetime-1000);
+
+        }, 1000);
+        return () => {
+            clearInterval(id);
+        }
+    },[datetime]);
+
+
+   
     return(
         
         <div className={styles.assignmentcontainer}>
@@ -21,7 +59,11 @@ export default function Assignment(assignment){
                     <h2>{assignment.title}</h2>
                 </div>
                 <div className={styles.assignmentfoot}>
-                    <p>{assignment.time.getHours()+ ":" + assignment.time.getMinutes() +":" + assignment.time.getSeconds()}</p>
+                    <p>
+                        {
+                            formatDate(datetime.toString())
+                        }
+                    </p>
                 </div>
             </div>
         </div>
