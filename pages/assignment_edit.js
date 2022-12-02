736b372af4cfbe70a4fbba8782114dfc243a714c
RauschElias <eliaz.rausch@gmail.com>
2022-12-02 11:01:26 +0100

    vgfggfgfg

---
+++ pages/assignment_edit.js
@@ -0,0 +1,7 @@
+import { useState,useEffect, useRef } from 'react'
+import Countdown from '../components/Countdown';
+import styles from '../styles/assignment.module.css'
+
+export default function AssignmentEdit({assignment}){
+    
+}
