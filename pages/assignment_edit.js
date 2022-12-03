31759544a9f96e5f97290c0dc0969aca73b86079
RauschElias <71702283+RauschElias@users.noreply.github.com>
2022-12-03 16:00:41 +0100

    working on fileupload

--- pages/assignment_edit.js
+++ pages/assignment_edit.js
@@ -13,14 +13,26 @@ export default function AssignmentEdit({assignment}){
             description: "dsasdasdadsadsad"
         }
 
-        useEffect(() => {
-            const fileupload = document.getElementById("fileuploadfield");
-            fileupload.addEventListener("drop",handleOnDrop());
+        const form = document.querySelector("form"),
+        fileInput = document.querySelector(".fileinput"),
+        progressArea = document.querySelector(".progressarea"),
+        uploadedArea = document.querySelector(".uploadedarea");
+        form.addEventListener("click", () =>{
+            fileInput.click();
         });
 
-        function handleOnDrop(e){
-            e.preve
+        fileInput.onchange = ({target})=>{
+        let file = target.files[0];
+        if(file){
+            let fileName = file.name;
+            if(fileName.length >= 12){
+            let splitName = fileName.split('.');
+            fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+            }
+            uploadFile(fileName);
         }
+        }
+
 
     return(
         <div className={styles.editcontainer}>
@@ -33,13 +45,30 @@ export default function AssignmentEdit({assignment}){
                     <p>{assignmentDummy.description}</p>
                 </div>
             </div>
-            <div className={styles.fileuploadcontainer}>
-                <div id="fileuploadfield" className={styles.fileuploadfield}>
-                    <label>Drag and Drop</label>
-                    <button>Choose File</button>
+
+            <div className={styles.uploadfieldcontainer}>
+                <div className={styles.wrapper}>
+                    <header>File Uploader JavaScript</header>
+                <form>
+                <input className={styles.fileinput} type="file" name="file" hidden></input>
+                    <i class="fas fa-cloud-upload-alt"></i>
+                    <p>Browse File to Upload</p>
+                    </form>
+                    <section className={styles.progressarea}></section>
+                    <section className={styles.uploadedarea}></section>
                 </div>
-                
             </div>
+            
+
+
+
+
+
+
+
+
+
+            
             <div className={styles.fileslist}>
                 <button>file1</button>
                 <button>file1</button>
