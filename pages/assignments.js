89c2e04bb60ab7074d65ba28da88028536874b35
RauschElias <71702283+RauschElias@users.noreply.github.com>
2022-12-15 20:39:39 +0100

    fixed error

--- pages/assignments.js
+++ pages/assignments.js
@@ -17,7 +17,6 @@ export default function Assignments(){
 
     return(
         <div className={styles.assignmentcontainer}>
-            <Image className={styles.cancelbutton} src={"/cancelicon.svg"} width={20} height={20} alt="cancel"></Image>
             {
                 assignmentData.map((element, i) => {
                     return <AssignmentCard key={i} assignment={element}></AssignmentCard>
