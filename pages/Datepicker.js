11d4bc794ffcd0fa7d906a0a49c160ff361c58da
RauschElias <71702283+RauschElias@users.noreply.github.com>
2022-12-20 19:24:31 +0100

    calc first and last day of month

--- pages/Datepicker.js
+++ pages/Datepicker.js
@@ -1,15 +1,12 @@
 import dayjs from 'dayjs';
 
-export default function Datepicker(startdate = 1){
-    let month;
-    let year;
+export default function Datepicker({month = new Date().getMonth(),year = new Date().getFullYear()}){
+    const firstDateOfMonth = dayjs().month(month).year(year).startOf("month");
+    const lastDateOfMonth = dayjs().month(month).year(year).endOf("month");
 
-    //month = new Date().getMonth();
-    year = new Date().getFullYear();
-    
+    const displayDatesArray = [firstDateOfMonth,lastDateOfMonth];
 
-    //const firstDateOfMonth = dayjs().month(month).year(year).startOf("month");
-    //const lastDateOfMonth = dayjs().month(month).year(year).endOf("month");
+    console.log(displayDatesArray);
 
-    console.log(startdate);
+   
 }
