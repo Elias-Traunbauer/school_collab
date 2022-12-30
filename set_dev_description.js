8a26020d5dd74b7833557c00b59d2496626624f7
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2022-12-30 22:39:09 +0100

    version

---
+++ set_dev_description.js
@@ -0,0 +1,17 @@
+var currentdate = new Date(); 
+var datetime = "Build " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + "-"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+const fs = require('fs');
+
+const content = "<?php echo '" + datetime + "'; ?>"
+
+fs.writeFile('./api/version.php', content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
