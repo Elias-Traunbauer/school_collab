136d20c1bcd8c5007f6e32fb90c8f43f3f53612a
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2022-12-30 23:06:44 +0100

    build date display

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,23 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import icon from '../public/icon.jpg'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
 
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('/api/version.php');
+      const data = await res.json();
+      const dev_info = document.getElementById('dev_info');
+      dev_info.innerHTML = "<p>" + data.dev_info + "</p>";
+    })();
+    return () => {
+      
+    };
+  }, []);
 
   return (
     <>
@@ -15,6 +28,7 @@ function MyApp({ Component, pageProps }) {
             <link rel="icon" href={icon.src} />
         </Head>
         <div id="dialog_container"></div>
+        <div id="dev_info"><p>No build date available</p></div>
       {
         getLayout(<Component {...pageProps} />)
       }
