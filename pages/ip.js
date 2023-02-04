38629d4d6b7fa60f64d5f455b4aaba2955ab0f82
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2023-02-04 17:44:38 +0100

    test

--- pages/ip.js
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server'
-
-export default function Ip() {
-    const clientIp = NextRequest().headers.get('x-real-ip') || NextRequest().headers.get('x-forwarded-for') || NextRequest().connection.remoteAddress
-    return (    <>
-        <h2>Your ip is: {clientIp}</h2>
-    </>)
-
-}
