ca5c8c87996cfe15d9a0d8f60916da3c88358b7b
RauschElias <eliaz.rausch@gmail.com>
2023-02-10 14:12:25 +0100

    chart fix

--- pages/dialog_test.js
+++ pages/dialog_test.js
@@ -2,11 +2,9 @@ import { showDecisionDialog } from "../components/Dialog";
 import { useEffect } from "react";
 
 export default function Test () {
-
     return (
         <>
             <button onClick={() => showDecisionDialog("Test", "This is the test with the number ", () => {}, () => {})}>Delete</button>
         </>
     );
-
 }
