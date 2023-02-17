7626853bf5fc84ed76a6b834a445d9e97c0ba0b2
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2023-02-17 15:08:47 +0100

    dadas

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         <Link href="/profile">Profile</Link>
         <Link type="special" href="/assignments">Assignments</Link>
         <Link href="/assignment_wizard">Assignment Wizard</Link>
-        <Link type="special" special href="/Datepicker">Datepicker</Link>
+        <Link type="special" href="/Datepicker">Datepicker</Link>
         <Link type="special" href="/dialog_test">Dialog Test</Link>
         <Link href="/about">About</Link>
     </div>
