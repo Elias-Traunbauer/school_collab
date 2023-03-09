f53ffe3ada499f88417d16e8e5de2f1a0c0de15a
foxhunterluca <luca.fuchsjaeger14@icloud.com>
2023-03-09 19:36:32 +0100

    show file upload after click circle set accepted file extentions

--- pages/profile.js
+++ pages/profile.js
@@ -1,19 +1,49 @@
 import File_Upload from "../components/file_upload";
+import {openDialog} from '../components/Dialog';
 import styles from "../styles/profilepic.module.css"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Profile ({ children }) {
-    useEffect(() => {
-        
-      });
-
+    const fileExtensions = ["jpeg", "jpg", "png"];
     const [showFileUpload, setShowFileUpload] = useState(false);
-    const onClick = () => setShowFileUpload(true);
+    const [uploadFile, setUploadFile] = useState([]);
+    const onClick = () => setShowFileUpload(!showFileUpload);
+    const [fileUploaded, setFileUploaded] = useState(false);
+
+    
+function handleFileUpdate(list){
+        if(list != null)
+        {
+            setUploadFile([...uploadFile, ...list]);
+            setFileUploaded(false);
+        }
+    }
+
+    /*function handleOpenDialog(id){
+        
+
+
+        openDialog(id);
+        document.getElementById("fileName").value = uploadFile.name;
+    }*/
 
     return( 
         <>
             <div className={styles.container} onClick={onClick}></div>
-            {showFileUpload ? <File_Upload/> : null}
+            {showFileUpload ? <File_Upload fileExtentions={fileExtensions} title={"Profile Picture"} handleFilesUpdated={fileUploaded ? (uploadFile) => handleFileUpdate(uploadFile) : handleFileUpdate(null)}/> : null}
+            
+            <div>
+                <div>
+                    {uploadFile.length > 0 ? 
+                    (<>
+                        <ul>
+                            <li id="fileName" onClick={() => handleOpenDialog("alo")}>
+                                <p>{fileUploaded ? null : uploadFile}</p>                                        
+                            </li>
+                        </ul>
+                    </>) : (<></>)}
+                </div>
+            </div>
         </>
     )
 }
