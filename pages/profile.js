fcf57cba31a492a8d34da251f69ecd4b7e3ddad8
foxhunterluca <luca.fuchsjaeger14@icloud.com>
2023-03-17 12:56:10 +0100

    external accounts of user listed on profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,9 @@
 import File_Upload from "../components/file_upload";
+import Image from "next/image";
 import Wizard from '../components/wizard';
 import styles from "../styles/profile.module.css"
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Profile() {
     let userDummy = {
@@ -10,11 +12,18 @@ export default function Profile() {
         lastName: "Frey",
     }
 
+    let newAccount = "";
+
+
+
     const fileExtensions = ["jpeg", "jpg", "png"];
     const [showFileUpload, setShowFileUpload] = useState(false);
     const [uploadFile, setUploadFile] = useState([]);
     const [fileUploaded, setFileUploaded] = useState(false);
     const [passwordChange, setPasswordChange] = useState(false);
+    const [links, setLinks] = useState(["https://www.google.com", "https://www.youtube.com"]);
+    const [linkAdded, setLinkAdded] = useState(false);
+    const [linkDeleted, setLinkDeleted] = useState(false);
 
     function picOnClick()
     {
@@ -62,22 +71,73 @@ export default function Profile() {
         //backend
       }
 
+      function printLinks(){
+            return links.map((link) => {
+                if(link != null)
+                    return <div><Link className={styles.Link} key={link} href={link} rel="noopener noreferrer" target="_blank">{link}</Link> 
+                                { linkDeleted ? <Image onClick={(e) => deleteItem(e,i)} className={styles.cancelbutton} src={"/cancelicon.svg"} width={20} height={20} alt="cancel"/> : null }
+                            </div>
+            })
+      }
+
+      function addLink(e){
+        e.preventDefault();
+        /*newAccount = prompt("Enter link");
+        addAccount(newAccount);*/
+
+        setLinkAdded(true);
+      }
+
+      function addLink(e){
+        e.preventDefault();
+
+        setLinkDeleted(true);
+      }
+
+      function addAccount(newAcc){
+            setLinks([...links, newAcc]);
+            setLinkAdded(false);
+      }
+      function stopAddAccount(){
+            setLinkAdded(false);
+      }
+
     return( 
         <>
             <div className={styles.container}>
                 <div className={styles.pic} onClick={picOnClick}></div>
 
-                <form className={styles.infos}>
-                    <label>Username: {userDummy.userName}</label>
-                    <label>Firstname: {userDummy.firstName}</label>
-                    <label>Lastname: {userDummy.lastName}</label>
-                    <button onClick={(e) => changePassword(e)}>change password</button>
-                </form> 
+                <div>
+                    <div className={styles.userData}>
+                        <form className={styles.infos}>
+                            <button onClick={(e) => changePassword(e)}>change password</button>
+                            <label>Username: {userDummy.userName}</label>
+                            <label>Firstname: {userDummy.firstName}</label>
+                            <label>Lastname: {userDummy.lastName}</label>
+                        </form> 
 
-                {passwordChange ? <Wizard callback={callback} containerWidth={20} contentData={contentData} title='Change password'/> : null} 
-            </div>   
-            
-            {(showFileUpload && !fileUploaded) ? <File_Upload fileExtentions={fileExtensions} title={"Profile Picture"} handleFilesUpdated={fileUploaded ? "" : (uploadFile) => handleFileUpdate(uploadFile)}/> : null}        
+                        <form className={styles.infos}>
+                            <div className={styles.infobutton}>
+                                <button onClick={(e) => addLink(e)}>link acc</button>
+                                <button onClick={(e) => deleteLink(e)}>delete acc</button>
+                            </div>
+                            
+                            {links != [] ? printLinks() : null}
+                            {linkAdded ? <div><input type="text" placeholder="Enter link" onChange={(e) => newAccount = e.target.value}/>
+                                <button onClick={() => addAccount(newAccount)}>add</button>
+                                <button onClick={() => stopAddAccount()}>cancel</button></div> : null}
+                            
+                        </form>
+                    </div>
+                    {passwordChange ? <Wizard callback={callback} containerWidth={20} contentData={contentData} title='Change password'/> : null}
+                </div>
+
+            </div>      
+
+            <div className={styles.fileUpload}>
+            {(showFileUpload && !fileUploaded) ? <File_Upload fileExtentions={fileExtensions} title={"Profile Picture"} handleFilesUpdated={fileUploaded ? "" : (uploadFile) => handleFileUpdate(uploadFile)}/> : null}
+            </div>
+                    
         </>
     )
 }
