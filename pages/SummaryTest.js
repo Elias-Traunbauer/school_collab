d3b9cb529e3c5904e2fabc3579d29df0e9874989
RauschElias <eliaz.rausch@gmail.com>
2023-04-14 08:20:02 +0200

    aufi

--- pages/SummaryTest.js
+++ pages/SummaryTest.js
@@ -1,10 +1,10 @@
-import SummaryPost from "../components/SummaryPost";
+import SummaryPostCard from "../components/SummaryPostCard";
 import styles from "../styles/Summary.module.scss";
 
 export default function SummaryTest() {
     return (
         <div className={styles.testContainer}>
-            <SummaryPost></SummaryPost>
+            <SummaryPostCard></SummaryPostCard>
         </div>
     )
 }
