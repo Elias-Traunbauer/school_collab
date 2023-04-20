0f627a3468241b5c4331ed65d78b82df4c9523f7
RauschElias <eliaz.rausch@gmail.com>
2023-04-20 14:43:03 +0200

    init

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import styles from '../styles/MarkdownEditor.module.scss';
-import Image from 'next/image';
-export default function MarkdownEditor({containerWidth = 50}) {
-    return(
-        <div style={{ width: containerWidth + '%' }} className={styles.container}>
-            <div className={styles.head}>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <Image src={"/expand.svg"} width={40} height={40} alt='heading'></Image>
-                <div className={`${styles.img} ${styles.imgHeading}`}></div>
-            </div>
-            <div className={styles.content}>
-                <textarea></textarea>
-            </div>
+import React from "react";
+import styles from "../styles/MarkdownEditor.module.scss";
+import Image from "next/image";
+export default function MarkdownEditor({ containerWidth = 50}) {
+    const [promode, setPromode] = React.useState(false);
+
+  return (
+    <div style={{ width: containerWidth + "%" }} className={styles.container}>
+      <div className={styles.head}>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgHeading}></div>
         </div>
-    );
+        <div className={`${styles.img}`}>
+          <div className={styles.imgCode}></div>
+        </div>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgLink}></div>
+        </div>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgBlockQuote}></div>
+        </div>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgBold}></div>
+        </div>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgItalic}></div>
+        </div>
+        <div className={`${styles.img}`}>
+          <div className={styles.imgBulletpoint}></div>
+        </div>
+        <div title="checkBox" className={`${styles.img}`}>
+          <div  className={styles.imgcheckBox}></div>
+        </div>
+        <div className={`${styles.img}`}>
+            <input onClick={()=>setPromode(!promode)} type="CheckBox"></input>
+                <span className={styles.tooltipText}>Tooltip text</span>
+        </div>
+       
+      </div>
+      <div className={styles.content}>
+        <textarea></textarea>
+      </div>
+    </div>
+
+  );
 }
