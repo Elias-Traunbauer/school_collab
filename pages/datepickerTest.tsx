3c8cf1e9ec3cd17c96234c66bc3e960c629b6918
RauschElias <eliaz.rausch@gmail.com>
2023-04-28 14:27:44 +0200

    datepicker fix

---
+++ pages/datepickerTest.tsx
@@ -0,0 +1,8 @@
+import react from 'react'
+import Datepicker from '../components/Datepicker'
+import React from 'react'
+export default function DatepickerTest() {
+    return (
+        <Datepicker OnInput={undefined}></Datepicker>
+    )
+}
