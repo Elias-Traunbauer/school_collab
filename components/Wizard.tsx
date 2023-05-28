403a054fc40753b1ebf5ef63e0fe2d77bd097ac9
RauschElias <71702283+RauschElias@users.noreply.github.com>
2023-05-28 11:30:52 +0200

    bug fix TODO: checkform polish

--- components/Wizard.tsx
+++ components/Wizard.tsx
@@ -3,14 +3,17 @@ import { useState, useEffect } from 'react'
 import Datepicker from './Datepicker';
 import React from 'react';
 import { WizardField } from '../models/WizardField';
+import MarkdownEditor from './MarkdownEditor';
+import WizardResult from '../models/WizardResult';
 
-export default function Wizard({ callback,contentData = [[new WizardField('firstname','checkBox',{value:true,text:'asdasdasdasd'},true),new WizardField('firstname','select',[{value:1,displayText:'1'},{value:1,displayText:'2'},{value:1,displayText:'3'}],true)],[new WizardField('date','date',new Date(),true)]], title = "Wizard", containerWidth = 50 }: { callback: Function, contentData?: WizardField[][], title: string, containerWidth?: number }) {
+export default function Wizard({ callback,contentData = [[new WizardField('checkBox','checkBox',{value:true,text:'asdasdasdasd'},true),new WizardField('select','select',[{value:1,displayText:'1'},{value:1,displayText:'2'},{value:1,displayText:'3'}],true)],[new WizardField('date','date',new Date(),true)]], title = "Wizard", containerWidth = 50 }: { callback: Function, contentData?: WizardField[][], title: string, containerWidth?: number }) {
         let inputList:HTMLInputElement[] = [];
     const [currentPage,setCurrentPage] = useState(0); 
     const [loadingText, setLoadingText] = useState("loading...");
+    const [valid, setValid] = useState(false);
 
     function displayPage(page){
-        console.log(page);
+        
         const items = document.querySelectorAll('.' + styles.wizzardContainer + ' ul li');
         const lines = document.querySelectorAll('.' + styles.wizardLine);
         const formList = document.querySelectorAll('.' + styles.wizardContent);
@@ -36,13 +39,11 @@ export default function Wizard({ callback,contentData = [[new WizardField('first
     function nextSection() {
         displayPage(currentPage+1);
         setCurrentPage(currentPage+1);
-        checkFormFilled(currentPage+1);
     }
 
     function previousSection() {
         displayPage(currentPage-1);
         setCurrentPage(currentPage-1);
-        checkFormFilled(currentPage-1);
     }
     function focusInputField(index) {
         const formList = document.querySelectorAll('.' + styles.wizardContent);
@@ -61,24 +62,26 @@ export default function Wizard({ callback,contentData = [[new WizardField('first
         for (let item of inputList) {
             
             if (item.hasAttribute('required') && item.type == 'checkbox' && !item.checked) {
-                btn.classList.add(styles.disabeldBtn);
+                console.log('checkbox');
+                setValid(false);
                 return;
             }
             else if (item.hasAttribute('required') && item.value.length <= 0) {
-                btn.classList.add(styles.disabeldBtn);
+                setValid(false);
                 return;
             }
         }
-        console.log(selectElements);
-        console.log(inputList);
+        
+        
         for (let item of selectElements) {
             if (item.hasAttribute('required') && item.value == '-1') {
-                btn.classList.add(styles.disabeldBtn);
+                
+                setValid(false);
                 return;
             }
         }
 
-        btn.classList.remove(styles.disabeldBtn);
+        setValid(true);
     }
 
     function finishWizard() {
@@ -106,8 +109,57 @@ export default function Wizard({ callback,contentData = [[new WizardField('first
         loader.classList.add(styles.finished);
     }
 
-    function getResult() {
-        return contentData;
+    function parseDate(dateString) {
+        const parts = dateString.split(' ');
+        const dateParts = parts[0].split('.');
+        const timeParts = parts[1].split(':');
+        
+        const day = parseInt(dateParts[0], 10);
+        const month = parseInt(dateParts[1], 10) - 1; // Months are zero-based in JavaScript
+        const year = parseInt(dateParts[2], 10);
+        const hour = parseInt(timeParts[0], 10);
+        const minute = parseInt(timeParts[1], 10);
+        
+        return new Date(year, month, day, hour, minute);
+      }
+
+    function getResult():WizardResult[] {
+        // get all forms
+        const inputContainerList = document.querySelectorAll('.' + styles.inputContainer);
+        let formIndex = 0;
+        let indx = 0;
+        const result:WizardResult[] = [];
+
+        for (const item of inputContainerList) {
+            const tmpRes:WizardResult = {
+                field:contentData[formIndex][indx],
+                value:''
+            };
+            if(contentData[formIndex][indx].type == 'checkBox' || contentData[formIndex][indx].type == 'checkbox' ){
+                tmpRes.value = (item.querySelector('input') as HTMLInputElement).checked;
+            }
+            else if(contentData[formIndex][indx].type == 'select'){
+                tmpRes.value = (item.querySelector('select') as HTMLSelectElement).value;
+            }
+            else if(contentData[formIndex][indx].type == 'date'){
+                tmpRes.value = parseDate((item.querySelector('input') as HTMLInputElement).value);
+            }
+            else if(contentData[formIndex][indx].type == 'markdown'){
+                tmpRes.value = (item.querySelector('textarea') as HTMLTextAreaElement).value;
+            }
+            else{
+                tmpRes.value = (item.querySelector('input') as HTMLInputElement).value;
+            }
+            if(indx == contentData[formIndex].length-1){
+                formIndex++;
+                indx = 0;
+            }
+            else{
+                indx++;
+            }
+            result.push(tmpRes);
+        }
+        return result;
     }
 
     function CancelWizard() {
@@ -115,20 +167,26 @@ export default function Wizard({ callback,contentData = [[new WizardField('first
     }
 
     function printInput(item:WizardField, formIndex:number, indx:number) {
-        console.log("Form " + formIndex);
+        
 
         return(
             <div key={'wzContent_'+formIndex + '_' + indx} className={styles.inputContainer}>
                 {
-                    item.type != 'checkBox'&&
+                    !(item.type == 'checkBox' || item.type == 'markdown' || item.type == 'checkbox' || item.type == 'md')&&
                     <label>{item.name}<span>{item.required&&'*'}</span></label>
                 }
             {
+                item.type == 'markdown' || item.type == 'md'?
+                <div className={styles.markdownContainer}>
+                    <MarkdownEditor containerWidth={100} isEditable={true}></MarkdownEditor>
+                </div>
+
+                :
                 item.type == 'date'?
                     <div className={styles.dateContainer}>
                     <Datepicker inputChanged={()=>checkFormFilled(formIndex)} title={item.name} dateParam={item.value} required={item.required}></Datepicker>
                     </div>
-                :item.type == 'checkBox'?
+                :item.type == 'checkBox' || item.type == 'checkbox'?
                     <div>
                         <input onInput={()=>checkFormFilled(formIndex)} type='checkbox' defaultChecked={item.value.defaultValue} required={item.required} />
                         <p><span>{item.required&& '* '}</span>{item.value.text}</p>
@@ -202,8 +260,8 @@ export default function Wizard({ callback,contentData = [[new WizardField('first
                         }
                     </div>
                     <div className={styles.wizardButtonContainer}>
-                        <button onClick={currentPage == 0 ? CancelWizard : previousSection}>{currentPage == 0 ? "Cancel" : "Back"}</button>
-                        <button id='btnNextPage' onClick={(e) => currentPage == contentData.length - 1 ? finishWizard() : nextSection()} className={styles.disabeldBtn}>{currentPage == contentData.length - 1 ? "Finish" : "Next"}</button>
+                        <button className={currentPage == 0?'btn btn-cancel':'btn btn-primary'} onClick={currentPage == 0 ? CancelWizard : previousSection}>{currentPage == 0 ? "Cancel" : "Back"}</button>
+                        <button disabled={valid} className='btn btn-primary' id='btnNextPage' onClick={currentPage == contentData.length - 1 ? finishWizard : nextSection}>{currentPage == contentData.length - 1 ? "Finish" : "Next"}</button>
                     </div>
                 </div>
             </div>
