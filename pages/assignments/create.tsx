c0fe455419a43053513462bcb2204a5324cbc62a
RauschElias <eliaz.rausch@gmail.com>
2023-06-02 14:40:09 +0200

    wizard update

--- pages/assignments/create.tsx
+++ pages/assignments/create.tsx
@@ -1,17 +1,30 @@
+import { useRouter } from "next/router";
 import Wizard from "../../components/Wizard";
 import WizardField from "../../models/WizardField";
+import WizardResult from "../../models/WizardResult";
 import styles from '../../styles/Assignment.module.scss'
 
 export default function CreateAssignment() {
+    const router = useRouter();
 
-    function nothing() {
-    }
+    function callback(data:WizardResult[], setLoadingText, finishLoading){
+        console.log(data);
+        setLoadingText("Penis...");
+        setTimeout(() => {
+            finishLoading();
+            setLoadingText("done!");
+
+            setTimeout(() => {
+                router.push('/assignments')
+            },300);
+        }, 2000);
+    };
 
     const data = [[new WizardField('title','text','',true),new WizardField('deadline','date',new Date(),true)],[new WizardField('description','md','',false)]]
 
     return (
         <div className={styles.CreationContainer}>
-            <Wizard contentData={data} callback={nothing} title={"Assignment"}></Wizard>
+            <Wizard contentData={data} callback={callback} title={"Assignment"}></Wizard>
         </div>
     )
 }
