a38acd54110373bc87c910c530a45adf47988226
foxhunterluca <71698785+foxhunterluca@users.noreply.github.com>
2023-09-29 09:40:10 +0200

    SIU

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,13 +43,3 @@ export default function Footer () {
         </>
     )
 }
-import styles from '../styles/Footer.module.scss'
-export default function Footer ({children}) {
-    return(
-        <>
-            <div className={styles.container}>
-                {children}
-            </div>
-        </>
-    )
-}
