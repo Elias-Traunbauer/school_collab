80428d384a22929b7d2d5c844e8b12c07926b7db
foxhunterluca <71698785+foxhunterluca@users.noreply.github.com>
2023-10-02 09:03:04 +0200

    profile page neustart

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,72 +1,24 @@
-import Wizard from '../../components/Wizard';
-import WizardField from '../../models/WizardField';
 import styles from "../../styles/Profile.module.scss";
 import { useState } from "react";
 import Image from "next/image";
-import AccountLinking from '../../components/AccountLinking';
 
 export default function Profile() {
-    let userDummy = {
-        userName: "bedabinguin",
-        firstName: "Peter",
-        lastName: "Frey",
-    }
-
-    const contentData = [[new WizardField('Type in your password','text',{value:true,text:'asdasdasdasd'},true),new WizardField('Type in your new password','text',[{value:1,displayText:'password'},{value:1,displayText:'süba'},{value:1,displayText:'süba'}],true)]]
-
-    const [passwordChange, setPasswordChange] = useState(false);
-
-    function changePasswordOnClick(e){
-        e.preventDefault();
-        setPasswordChange(true);
-    }
-
-
-    function openFileExplorer(){
-        document.getElementById('file').click();
-    }
-
-    function handleFileChanged(e){
-        var profilePic = document.getElementById('pic');
-
-        profilePic.setAttribute('src',URL.createObjectURL(e.target.files[0]));
-    }
-    
-    function callback(){
-        setPasswordChange(false);
-    }
 
     return( 
         <>
-        <div className={styles.page}>
             <div className={styles.container}>
-                <div className={styles.container}>
-                <input type="file" id="file" hidden={true} onChange={(e) => handleFileChanged(e)}/>
-
-                <Image id='pic' width={20} height={20} alt='Pic' src='/cancelicon.svg' onClick={openFileExplorer} className={styles.pic}>
-
-                </Image>    
-                <button className='btn' onClick={openFileExplorer}>Change</button>
+                <div className={styles.picContainer}>
+                    <Image src={"/jpg.svg"} alt="cancel" width={150} height={150} />
                 </div>
-
-                <div>
+                <div className={styles.infoContainer}>    
                     <div>
-                        <form className={styles.infos}>
-                            <label htmlFor='username'><span>Username:</span></label>
-                            <input value={userDummy.userName} id='username'/>
-                            <label htmlFor='firstName'><span>Firstname:</span></label>
-                            <input value={userDummy.firstName} id='firstName'/>
-                            <label htmlFor='lastName'><span>Lastname:</span></label>
-                            <input value={userDummy.lastName} id='lastName'/>
-                            <button className='btn' onClick={(e) => changePasswordOnClick(e)}>change password</button>
-                        </form> 
+                        hallo
                     </div>
-                    {passwordChange ? <Wizard containerWidth={5} returnPath='/user/profile' callback={() => callback()} contentData={contentData} title='Change password'/> : null}
+                    <div>
+                        hallo
+                    </div>
                 </div>
-
             </div>
-        </div>
-        <AccountLinking/>
         </>
     )
 }
