fb15bd11e1addf6eebaacc5b1812a844067754fb
foxhunterluca <71698785+foxhunterluca@users.noreply.github.com>
2023-10-17 10:59:02 +0200

    link accounts

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,7 +1,10 @@
 import styles from "../../styles/Profile.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Wizard from "../../components/Wizard";
+import WizardField from "../../models/WizardField";
+import { json } from "stream/consumers";
 
 export default function Profile() {
     const user = {
@@ -11,11 +14,23 @@ export default function Profile() {
         class: "5BHIF",
     }
 
-    const [links, setLinks] = useState(["github", "google", "yahoo", "bing"]);
+    const data = [[new WizardField('Plattform', 'text', '', true),new WizardField('Link zu Ihrem Profil', 'text', '', true)]];
 
-    function removeLink(link: string){
+    const [links, setLinks] = useState([["github", "www.github.com"], ["google", "www.google.com"], ["yahoo", "www.yahoo.com"], ["bing", "www.bing.com"]]);
+    const [addLink, setAddLink] = useState(false);
+
+    function removeLink(link: string[]){
         const newLinks = links.filter((l) => l !== link);
         setLinks(newLinks);
+    }
+
+    function wizardCallback(result, callbackLoadingText, finishLoading){
+        var newLinks = links;
+
+        newLinks.push([result[0].value, result[1].value]);
+        setLinks(newLinks);
+
+        setAddLink(false);
     }
 
     return( 
@@ -24,6 +39,13 @@ export default function Profile() {
                 <div className={styles.picContainer}>
                     <Image src={"/jpg.svg"} alt="cancel" width={150} height={150} />
                 </div>
+
+                {addLink && (
+                    <div className={styles.wizard}>
+                        <Wizard returnPath="/user/profile" callback={wizardCallback} contentData={data} title="Account hinzufügen"></Wizard>
+                    </div>
+                )}
+
                 <div className={styles.infoContainer}>    
                     <div>
                         {Object.values(user).map((value, index) => {
@@ -37,22 +59,21 @@ export default function Profile() {
                         })}
                     </div>
                     <div>
-                        <div>
-                            <button>+</button>
-                        </div>
-
                         {links.map((link, index) => {
                             return (
                                 <div className={styles.linkContainer} key={index}>
                                     <Image src={"/jpg.svg"} width={20} height={20} alt="cancel"></Image>
-                                    <label>{link}</label>
-                                    <p>{link}_Name</p>
+                                    <Link href={"https://" + link[1]} target="blank">{link[0]}</Link>
                                     <button className="btn-primary" onClick={() => removeLink(link)}>X</button>
                                 </div>
                             )
                             
                         }
                         )}
+
+                        <div>
+                            <button onClick={() => setAddLink(true)}>+</button>
+                        </div>
                     </div>
                 </div>
             </div>
