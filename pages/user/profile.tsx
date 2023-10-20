cc19b99d78de8dd46b578ba605f8288ec53d501a
foxhunterluca <71698785+foxhunterluca@users.noreply.github.com>
2023-10-20 10:41:35 +0200

    links finished

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,5 +1,5 @@
 import styles from "../../styles/Profile.module.scss";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Wizard from "../../components/Wizard";
@@ -21,33 +21,72 @@ export default function Profile() {
 
     function removeLink(link: string[]){
         const newLinks = links.filter((l) => l !== link);
+        
         setLinks(newLinks);
     }
 
-    function wizardCallback(result, callbackLoadingText, finishLoading){
+    function handleSubmit(e){
+        e.preventDefault();
         var newLinks = links;
 
-        newLinks.push([result[0].value, result[1].value]);
-        setLinks(newLinks);
+        const inputs = document.querySelectorAll(
+            "input[type=text]"
+        ) as unknown as HTMLInputElement[];
 
+        newLinks.push([inputs[0].value, inputs[1].value]);
+        setLinks(newLinks);
         setAddLink(false);
+    }
+
+    function openFileExplorer(){
+        const fileInput = document.getElementById("upload");
+        fileInput.click();
+    }
+
+    function handleInputChange(e){
+        if(!e.target.files || e.target.files.length === 0) return console.log("No file selected");
+
+        const file = e.target.files[0];
+        const pic = document.getElementById("profilePic");
+        pic.setAttribute("src", URL.createObjectURL(file));
     }
 
     return( 
         <>
             <div className={styles.container}>
                 <div className={styles.picContainer}>
-                    <Image src={"/jpg.svg"} alt="cancel" width={150} height={150} />
+                    <Image id="profilePic" src={"/jpg.svg"} alt="cancel" width={150} height={150} onClick={openFileExplorer}/>
+                    <input id="upload" type="file" hidden onChange={handleInputChange}></input>
                 </div>
+
+                {}
 
                 {addLink && (
                     <div className={styles.wizard}>
-                        <Wizard returnPath="/user/profile" callback={wizardCallback} contentData={data} title="Account hinzufügen"></Wizard>
+                        <form onSubmit={handleSubmit}>
+                            <h1>Account hinzufügen</h1>
+
+                            <div className={styles.inputfield}>
+                                <label>Plattform*</label>
+                                <p>z.B. GitHub</p>
+                                <input required type="text" placeholder="Plattform" />
+                            </div>
+
+                            <div className={styles.inputfield}>
+                                <label>Link zu Ihrem Account*</label>
+                                <p>z.B. www.github.com/username</p>
+                                <input required type="text" placeholder="Link zu Ihrem Account" />  
+                            </div>
+
+                            <div className={styles.buttonContainer}>
+                                <input type="submit" value={"Speichern"} className="btn-primary"></input>
+                            </div>
+                        </form>
                     </div>
                 )}
 
-                <div className={styles.infoContainer}>    
-                    <div>
+                <div className={styles.infoContainer}> 
+                <div>
                         {Object.values(user).map((value, index) => {
                             return (
                                 <div key={index}>
@@ -64,7 +103,7 @@ export default function Profile() {
                                 <div className={styles.linkContainer} key={index}>
                                     <Image src={"/jpg.svg"} width={20} height={20} alt="cancel"></Image>
                                     <Link href={"https://" + link[1]} target="blank">{link[0]}</Link>
-                                    <button className="btn-primary" onClick={() => removeLink(link)}>X</button>
+                                    <button className="btn-primary" onClick={() => removeLink(link)} disabled={addLink}>X</button>
                                 </div>
                             )
                             
