43258f4c42a874352b6abd4e844acc5a2a383bff
foxhunterluca <71698785+foxhunterluca@users.noreply.github.com>
2023-11-21 09:08:08 +0100

    nix

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,4 +1,4 @@
-import styles from "../../styles/Profile.module.scss";
+import styles from "../../styles/Profile.module.css";
 import { use, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
