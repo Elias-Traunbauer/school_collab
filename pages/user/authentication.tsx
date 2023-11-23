46b48acbc2a16b9cd024f4ee5aea498fda1865f1
RauschElias <eliaz.rausch@gmail.com>
2023-11-23 15:24:13 +0100

    crazy

--- pages/user/authentication.tsx
+++ pages/user/authentication.tsx
@@ -2,13 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script';
 import { useState } from 'react'
-import styles from '../../styles/otp.module.css'
+import styles from '../../styles/otp.module.scss'
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { twoFactorAuthenticationCode } from '../../services/User.service';
 
 
 export default function Otp(){
 
     //const [inputs,setInputs] = useState();
+    const router = useRouter();
 
     useEffect(() => {
         const inputs = document.querySelectorAll(`.${styles.otpcontainer} input`);
@@ -80,7 +83,10 @@ export default function Otp(){
             }
         }
 
-        curr.focus();
+        if(curr.value.length > 0 && curr.nextElementSibling != null){
+            const button = document.getElementById('verifyBtn');
+            button.focus();
+        }
         //inputs[inputs.length-1].focus();
     }
 
@@ -115,6 +121,10 @@ export default function Otp(){
             input.value = e.key;
             if(input.nextElementSibling!= null)
             input.nextElementSibling.focus();
+            else{
+                const button = document.getElementById('verifyBtn');
+                button.focus();
+            }
         }
 
         /*if(e.key === "Backspace" && input.value.length <= 0 && input.previousElementSibling != null){
@@ -127,9 +137,44 @@ export default function Otp(){
         //input.nextElementSibling.focus();
     }
 
+    function handleSend2FA(e){
+        const button = e.target as HTMLButtonElement;
+        button.disabled = true;
+        button.classList.add(styles.loading);
+        const span = button.querySelector("span");
+        span.textContent = "";
+
+
+        const inputs = document.querySelectorAll(`.${styles.otpcontainer} input`) as NodeListOf<HTMLInputElement>;
+        var code = "";
+        for (const iterator of inputs) {
+            code += iterator.value;
+        }
+
+        twoFactorAuthenticationCode(code).then(res => {
+            span.textContent = "Verify";
+            button.classList.remove(styles.loading);
+            button.disabled = false;
+        }).catch(err => {
+            span.textContent = "Retry";
+            button.classList.remove(styles.loading);
+            button.classList.add(styles.failed);
+            button.disabled = false;
+        });
+
+        setTimeout(() => {
+            
+        }, 2000);
+    }
+
     return(
         <>
-
+            <div className={styles.container}>
+            <div className={styles.header}>
+                    <Image onClick={router.back} width={10} height={10} alt='return' src={"/arrow_right_alt.svg"}></Image>
+                    <h1>2 Factor Authentification</h1>
+            </div>
+            <div className={styles.wrapper}>
             <div  className={styles.otpcontainer}>
                 <input autoComplete='off' id='i1' maxLength={1} type="text"></input>
                 <input autoComplete='off' id='i2' maxLength={1} type="text"></input>
@@ -138,6 +183,13 @@ export default function Otp(){
                 <input autoComplete='off' id='i5' maxLength={1} type="text"></input>
                 <input autoComplete='off' id='i6' maxLength={1} type="text"></input>
             </div>
+            <div className={styles.buttonContainer}>
+                <button id='verifyBtn' onClick={handleSend2FA} className='btn btn-primary'><span>Verify</span></button>
+            </div>
+            </div>
+            
+            </div>
+            
         </>
     
     );
