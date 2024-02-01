d39230f556c94bccef97f7ce17f3a3dbdb57859d
Trauniii <59607758+Elias-Traunbauer@users.noreply.github.com>
2024-02-01 18:33:19 +0100

    yes

--- Graduater/Core/Entities/Database/AssignmentFile.cs
+++ Graduater/Core/Entities/Database/AssignmentFile.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Core.Entities.Database
 {
-    public class AssignmentFile
+    public class AssignmentFile : DatabaseEntity
     {
         public int Id { get; set; }
 
